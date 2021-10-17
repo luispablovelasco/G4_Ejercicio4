b0aefbbb1ccb22a92fcdd8c0b7bb846146bb79b6
luispablovelasco <pablo.velascoflores@gmail.com>
2021-10-17 17:02:44 -0600

    Creacion de clase abstracta y clases heredadas con un formulario principal

---
+++ G4_Ejercicio4/G4_Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace G4_Ejercicio4
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnproceso = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ejercicio 4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(98, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Guia 4";
+            // 
+            // btnproceso
+            // 
+            this.btnproceso.Location = new System.Drawing.Point(64, 211);
+            this.btnproceso.Name = "btnproceso";
+            this.btnproceso.Size = new System.Drawing.Size(101, 41);
+            this.btnproceso.TabIndex = 2;
+            this.btnproceso.Text = "Ir al programa";
+            this.btnproceso.UseVisualStyleBackColor = true;
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(204, 211);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(101, 41);
+            this.btnsalir.TabIndex = 3;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::G4_Ejercicio4.Properties.Resources.unnamed;
+            this.pictureBox1.Location = new System.Drawing.Point(251, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(394, 310);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnproceso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FrmMenu";
+            this.Text = "Ejercicio 4 (Guia 4)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnproceso;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ G4_Ejercicio4/G4_Ejercicio4/Vehiculo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G4_Ejercicio4
+{
+    abstract class Vehiculo
+    {
+
+        string tipomotor;
+        string transmision;
+        int llantas;
+        int pasajeros;
+        double masa;
+        string combustible;
+        int sizerin;
+
+        public string Tipomotor
+        {
+            get { return tipomotor; }
+            set { tipomotor = value; }
+        }
+
+        public string Transmision
+        {
+            get { return transmision; }
+            set { transmision = value; }
+        }
+
+        public int Llantas
+        {
+            get { return llantas; }
+            set { llantas = value; }
+        }
+
+        public int Pasajeros
+        {
+            get { return pasajeros; }
+            set { pasajeros = value; }
+        }
+
+        public double Masa
+        {
+            get { return masa; }
+            set { masa = value; }
+        }
+
+        public string Combustible
+        {
+            get { return combustible; }
+            set { combustible = value; }
+        }
+
+        public int Rines
+        {
+            get { return sizerin; }
+            set { sizerin = value; }
+        }
+    }
+}
